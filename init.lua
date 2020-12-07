54ffe79e51c706b96e7bbc850623bf511d4ab1a2
MihinMUD <73010893+MihinMUD@users.noreply.github.com>
2020-12-07 10:15:14 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,9 @@
 minetest.register_node("mud:mud", {
     description = "Mud",
+    drawtype = "liquid",
+	waving = 3,
     tiles = {"mud.png"},
-    groups = {crumbly = 3, soil = 1},
+    groups = {water = 3, liquid = 3, cools_lava = 1,crumbly = 3, soil = 1},
     sounds = {
         footstep = {name = "mud"},
         dug = {name = "mud"}   

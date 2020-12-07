1970bc0d1bb4bd1a9db97cc147cc97021f9e31fb
MihinMUD <73010893+MihinMUD@users.noreply.github.com>
2020-12-07 09:07:42 +0530

    Add files via upload

---
+++ mud/init.lua
@@ -0,0 +1,14 @@
+minetest.register_node("mud:mud", {
+    description = "Mud",
+    tiles = {"mud.png"},
+    groups = {crumbly = 3, soil = 1},
+    sounds = {
+        footstep = {name = "mud"}
+        dug = {name = "mud"}   
+},
+})
+
+minetest.register_craft({
+    output = "mud:mud",
+    recipe = {{"default:dirt" , "default:dirt" }}
+})

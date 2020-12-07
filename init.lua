6124c1c86daa8d260298b549e08691240c4e5579
MihinMUD <73010893+MihinMUD@users.noreply.github.com>
2020-12-07 11:17:32 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,6 +6,40 @@ minetest.register_node("mud:mud", {
         footstep = {name = "mud"},
         dug = {name = "mud"}   
 },
+paramtype = "light",
+walkable = false,
+pointable = false,
+diggable = true,
+buildable_to = true,
+is_ground_content = false,
+drop = "",
+drowning = 1,
+liquidtype = "source",
+liquid_alternative_flowing = "mud:mud_flowing",
+liquid_alternative_source = "mud:mud",
+liquid_viscosity = 1,
+})
+
+minetest.register_node("mud:mud_flowing", {
+    description = "Mud flowing",
+    tiles = {"mud.png"},
+    groups = {water = 3, liquid = 3, cools_lava = 1,crumbly = 3, soil = 1},
+    sounds = {
+        footstep = {name = "mud"},
+        dug = {name = "mud"}   
+},
+paramtype = "light",
+walkable = false,
+pointable = false,
+diggable = true,
+buildable_to = true,
+is_ground_content = false,
+drop = "",
+drowning = 1,
+liquidtype = "flowing",
+liquid_alternative_flowing = "mud:mud_flowing",
+liquid_alternative_source = "mud:mud",
+liquid_viscosity = 1,
 })
 
 minetest.register_craft({

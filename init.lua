8995bcfffa6b9d9f03e665f6bce45f8a06ab24c4
TechDudie <73961295+TechDudie@users.noreply.github.com>
2020-12-08 15:54:28 -0600

    No flowing mud
    You can think about approving this PR, I don’t mind, just I don’t think flowing mud is a good idea. And I’ve noticed, flowing mud is no use, it’s the same texture.

--- init.lua
+++ init.lua
@@ -16,30 +16,6 @@ minetest.register_node("mud:mud", {
     drop = "",
     drowning = 1,
     liquidtype = "source",
-    liquid_alternative_flowing = "mud:mud_flowing",
-    liquid_alternative_source = "mud:mud",
-    liquid_viscosity = 1,
-})
-
-minetest.register_node("mud:mud_flowing", {
-    description = "Mud flowing",
-    tiles = {"mud.png"},
-    groups = {water = 3, liquid = 3, cools_lava = 1,crumbly = 3, soil = 1},
-    post_effect_color = {a = 412, r = 81, g = 42, b = 8},
-    sounds = {
-        footstep = {name = "mud"},
-        dug = {name = "mud"}   
-},
-    paramtype = "light",
-    walkable = false,
-    pointable = true,
-    diggable = true,
-    buildable_to = true,
-    is_ground_content = false,
-    drop = "",
-    drowning = 1,
-    liquidtype = "flowing",
-    liquid_alternative_flowing = "mud:mud_flowing",
     liquid_alternative_source = "mud:mud",
     liquid_viscosity = 1,
 })
@@ -47,9 +23,4 @@ minetest.register_node("mud:mud_flowing", {
 minetest.register_craft({
     output = "mud:mud",
     recipe = {{"default:dirt" , "default:dirt" }}
-})
-
-minetest.register_craft({
-    output = "default:diamond",
-    recipe = {{"mud:mud" }}
 })
